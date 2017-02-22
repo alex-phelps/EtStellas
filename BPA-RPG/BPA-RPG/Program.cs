d4609aaca8f1e745a8b1bfe6e85d6ae0f34fe89d
BuildTools <amp20123@gmail.com>
2017-02-22 16:58:36 -0500

    Stupid ClickOnce hates me

--- BPA-RPG/BPA-RPG/Program.cs
+++ BPA-RPG/BPA-RPG/Program.cs
@@ -17,9 +17,6 @@ namespace EtStellas
         [STAThread]
         static void Main()
         {
-            using (var game = new MainGame())
-                game.Run();
-
             try
             {
                 using (var game = new MainGame())
